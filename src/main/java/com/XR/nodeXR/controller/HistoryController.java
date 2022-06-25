92be879aa4bea25eb401c23ffb297e3887f82a1e
narendranath gogineni <narenrockstar1@gmail.com>
2022-06-26 00:04:20 +0530

    Reactive fileUpload fix

--- src/main/java/com/XR/nodeXR/controller/HistoryController.java
+++ src/main/java/com/XR/nodeXR/controller/HistoryController.java
@@ -1,5 +1,6 @@
 package com.XR.nodeXR.controller;
 
+import com.XR.nodeXR.repository.HistoryRepository;
 import com.XR.nodeXR.resolver.HistoryResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,25 +9,26 @@ import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/history")
 public class HistoryController {
-    private HistoryResolver historyResolver;
+    private final HistoryRepository historyRepository;
 
-    public HistoryController() {
-        this.historyResolver = new HistoryResolver();
+    public HistoryController(HistoryRepository historyRepository) {
+        this.historyRepository = historyRepository;
     }
 
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @ResponseBody
     public ResponseEntity uploadHistory(@RequestParam("browser") String browser, @RequestPart("myFile") FilePart file) {
         try {
-            historyResolver.historyUploader(file);
-        } catch (IOException e) {
-//            File fileToBeDeleted = new File("/home/vennela/Downloads/tmp/" + file.filename());
-//            fileToBeDeleted.delete();
+            HistoryResolver historyResolver = new HistoryResolver(historyRepository, file);
+            historyResolver.uploadFile();
+            historyResolver.processFile();
+        } catch (Exception e) {
+            File fileToBeDeleted = new File("/home/vennela/Downloads/tmp/" + file.filename());
+            fileToBeDeleted.delete();
             return new ResponseEntity("Unable To Process the file", HttpStatus.EXPECTATION_FAILED);
         }
         return new ResponseEntity("File Uploaded SuccessFully", HttpStatus.OK);
--- src/main/java/com/XR/nodeXR/resolver/HistoryResolver.java
+++ src/main/java/com/XR/nodeXR/resolver/HistoryResolver.java
@@ -1,18 +1,36 @@
 package com.XR.nodeXR.resolver;
 
-import com.XR.nodeXR.service.HistoryProcessor;
+import com.XR.nodeXR.model.History;
+import com.XR.nodeXR.repository.HistoryRepository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.codec.multipart.FilePart;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class HistoryResolver {
+    private final HistoryRepository historyRepository;
+    private final FilePart file;
 
-    public void historyUploader(FilePart file) throws IOException {
-        file.transferTo(new File("/home/vennela/Downloads/tmp/" + file.filename())).block(Duration.ofSeconds(5));
-        HistoryProcessor historyProcessor = new HistoryProcessor(file.filename());
-        historyProcessor.processFile();
+    public HistoryResolver (HistoryRepository historyRepository, FilePart file) {
+        this.historyRepository = historyRepository;
+        this.file = file;
+    }
+
+    public void uploadFile() {
+        file.transferTo(new File("/home/vennela/Downloads/tmp/" + file.filename())).subscribe();
+    }
+
+    public void processFile() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        FileInputStream inputFile = new FileInputStream("/home/vennela/Downloads/tmp/" + file.filename());
+        List<History> historyList = Arrays.asList(mapper.readValue(inputFile, History[].class));
+        for(History historyItem : historyList) {
+            historyRepository.save(historyItem).subscribe(item -> System.out.println(item.getTitle()));
+        }
     }
 }
